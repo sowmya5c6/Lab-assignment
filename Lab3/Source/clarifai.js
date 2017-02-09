8f0406c76e523b4595f9a7ae5dde0635828da62e
sowmya5c6 <syb7c@umkc.edu>
2017-02-08 22:11:27 -0600

    comitted

---
+++ Lab3/Source/clarifai.js
@@ -0,0 +1,93 @@
+function getCredentials(cb) {
+    var data = {
+        'grant_type': 'client_credentials',
+        'client_id': CLIENT_ID,
+        'client_secret': CLIENT_SECRET
+    };
+    var url = 'https://api.clarifai.com/v1/token';
+
+    return axios.post(url, data, {
+        'transformRequest': [
+            function() {
+                return transformDataToParams(data);
+            }
+        ]
+    }).then(function(r) {
+        localStorage.setItem('accessToken', r.data.access_token);
+        localStorage.setItem('tokenTimestamp', Math.floor(Date.now() / 1000));
+        cb();
+    }, function(err) {
+        console.log(err);
+    });
+}
+
+function transformDataToParams(data) {
+    var str = [];
+    for (var p in data) {
+        if (data.hasOwnProperty(p) && data[p]) {
+            if (typeof data[p] === 'string'){
+                str.push(encodeURIComponent(p) + '=' + encodeURIComponent(data[p]));
+            }
+            if (typeof data[p] === 'object'){
+                for (var i in data[p]) {
+                    str.push(encodeURIComponent(p) + '=' + encodeURIComponent(data[p][i]));
+                }
+            }
+        }
+    }
+    return str.join('&');
+}
+
+function postImage(imgurl) {
+    var accessToken = localStorage.getItem('accessToken');
+    var data = {
+        'url': imgurl
+    };
+    var url = 'https://api.clarifai.com/v1/tag';
+    return axios.post(url, data, {
+        'headers': {
+            'Authorization': 'Bearer ' + accessToken
+        }
+        /*'content-type': 'application/x-www-form-urlencoded'*/
+    }).then(function(r) {
+        parseResponse(r.data);
+    }, function(err) {
+        console.log('Sorry, something is wrong: ' + err);
+    });
+}
+
+function parseResponse(resp) {
+    var tags = [];
+    if (resp.status_code === 'OK') {
+        var results = resp.results;
+        tags = results[0].result.tag.classes;
+    } else {
+        console.log('Sorry, something is wrong.');
+    }
+    var val = document.getElementById('imgurl').value,
+        img = document.createElement('img');
+    img.src = val;
+    img.width=100;
+    img.height=100;
+    val.height=100;
+    val.width=10;
+
+    document.getElementById('data_for_img').innerHTML='Retrieved tags';
+    document.getElementById('data_bef_img').innerHTML='Given Image';
+    document.getElementById('check').innerHTML = "<img src='"+val+"' height='200' width='300'/>";
+    document.getElementById('tags').innerHTML = tags.toString().replace(/,/g, ', ');
+    var msg = new SpeechSynthesisUtterance('Tags related to the given image are'+tags);
+    window.speechSynthesis.speak(msg);
+}
+
+
+function run(imgurl) {
+    if (Math.floor(Date.now() / 1000) - localStorage.getItem('tokenTimeStamp') > 86400
+        || localStorage.getItem('accessToken') === null) {
+        getCredentials(function() {
+            postImage(imgurl);
+        });
+    } else {
+        postImage(imgurl);
+    }
+}
---
+++ Lab3/Source/js/index2.js
@@ -0,0 +1,4 @@
+<script>
+var CLIENT_ID = '9IQGQoqLGQbgBFaVM9Agf5jvvVeHarPw4a7MocmH';
+var CLIENT_SECRET = 'vt0cPE779rivSAm9piip3n-nEdGYgO-QxBQyKno7';
+</script>
